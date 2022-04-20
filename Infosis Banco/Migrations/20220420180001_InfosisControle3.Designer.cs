42550087fb0a4755b476dfcc9a12ef4138dd2461
João Victor <lemelting53@gmail.com>
2022-04-20 15:00:27 -0300

    migration adicionada para atualizar o banco de dados

--- Infosis Banco/Migrations/20220420180001_InfosisControle3.Designer.cs
+++ Infosis Banco/Migrations/20220420180001_InfosisControle3.Designer.cs
@@ -4,6 +4,7 @@ using Infosis_Banco;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infosis_Banco.Migrations
 {
     [DbContext(typeof(AppContext))]
-    partial class AppContextModelSnapshot : ModelSnapshot
+    [Migration("20220420180001_InfosisControle3")]
+    partial class InfosisControle3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
