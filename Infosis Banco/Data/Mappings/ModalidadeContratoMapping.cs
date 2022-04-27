5796c8c94932928c24b145f3ba91005163f44702
João Victor <lemelting53@gmail.com>
2022-04-27 16:30:00 -0300

    implementando interface modalidadecontrato

--- Infosis Banco/Data/Mappings/ModalidadeContratoMapping.cs
+++ Infosis Banco/Data/Mappings/ModalidadeContratoMapping.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Infosis_Banco.Data.Mappings
 {
-    internal class ModalidadeContratoMapping
+    public class ModalidadeContratoMapping : IEntityTypeConfiguration<ModalidadeContrato>
     {
+        public void Configure(EntityTypeBuilder<ModalidadeContrato> builder)
+        {
+        }
     }
 }
