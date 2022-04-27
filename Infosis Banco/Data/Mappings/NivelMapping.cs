7b4f2c9d39082ad394b72c8737b695dfb922a07b
João Victor <lemelting53@gmail.com>
2022-04-27 16:28:58 -0300

    implementando interface nivel

--- Infosis Banco/Data/Mappings/NivelMapping.cs
+++ Infosis Banco/Data/Mappings/NivelMapping.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Infosis_Banco.Data.Mappings
 {
-    internal class NivelMapping
+    internal class NivelMapping : IEntityTypeConfiguration<Nivel>
     {
+        public void Configure(EntityTypeBuilder<Nivel> builder)
+        {
+        }
     }
 }
