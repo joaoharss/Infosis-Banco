68662586b9236e8a545e3ff8b3247c8a0f9f6d60
João Victor <lemelting53@gmail.com>
2022-04-27 16:26:16 -0300

    terminando de adicionar as classes de mapping

---
+++ Infosis Banco/Data/Mappings/NivelMapping.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infosis_Banco.Data.Mappings
+{
+    internal class NivelMapping
+    {
+    }
+}
