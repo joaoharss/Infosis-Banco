69d405c1ce2e32c53ae32d0098f8f790547426cf
João Victor <lemelting53@gmail.com>
2022-04-27 16:30:42 -0300

    implementando interface deposito

--- Infosis Banco/Data/Mappings/DepositoMapping.cs
+++ Infosis Banco/Data/Mappings/DepositoMapping.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Infosis_Banco.Data.Mappings
 {
-    internal class DepositoMapping
+    public class DepositoMapping : IEntityTypeConfiguration<Deposito>
     {
+        public void Configure(EntityTypeBuilder<Deposito> builder)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
