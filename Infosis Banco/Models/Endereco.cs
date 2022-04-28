3463f98bb196b13b5a3e99f0f4c72117621ee525
João Victor <lemelting53@gmail.com>
2022-04-28 14:47:07 -0300

    Criando nova tabela endereco

---
+++ Infosis Banco/Models/Endereco.cs
@@ -0,0 +1,15 @@
+﻿namespace Infosis_Banco
+{
+    public class Endereco
+    {
+        public int Id { get; set; }
+        public string Rua { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string? Numero { get; set; }
+        public string CEP { get; set; }
+        public char UF { get; set; }
+        public IEnumerable<Funcionario> Funcionarios{ get; set; }
+
+    }
+}
