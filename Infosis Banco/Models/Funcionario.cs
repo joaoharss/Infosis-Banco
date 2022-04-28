2f245a32d2a63ce71b3b0f9234d66a36a6be3593
João Victor <lemelting53@gmail.com>
2022-04-28 14:48:04 -0300

    apropriando endereco como uma FK de funcionario

--- Infosis Banco/Models/Funcionario.cs
+++ Infosis Banco/Models/Funcionario.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
+using Infosis_Banco;
 
 namespace Infosis_Banco
 {
@@ -7,13 +8,15 @@ namespace Infosis_Banco
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Sobrenome { get; set; }
-        public string Endereco { get; set; }
         public long Telefone { get; set; }
         public string CPF { get; set; }
         [ForeignKey("ModalidadeCargo")]
         public int ModalidadeCargoId { get; set; }
         public virtual ModalidadeCargo ModalidadeCargo{ get; set; }
         public IEnumerable<DepositoBeneficio> DepositoBeneficios { get; set; }
+        [ForeignKey("Endereco")]
+        public int EnderecoId { get; set; }
+        public virtual Endereco Endereco { get; set; }
 
     }
 }
